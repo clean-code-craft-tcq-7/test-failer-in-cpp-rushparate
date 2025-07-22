650affe84d1792bd601371211fa1499e4ab3511c
rushparate <rushaliparate@gmail.com>
2025-07-22 22:52:45 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -3,12 +3,15 @@
 
 char size(int cms) {
     char sizeName = '\0';
-    if(cms < 38) {
+    if(cms <= 38) {
         sizeName = 'S';
-    } else if(cms > 38 && cms < 42) {
+    } else if(cms > 38 && cms <= 42) {
         sizeName = 'M';
-    } else if(cms > 42) {
+    } else if(cms > 42 && cms <= 46) {
         sizeName = 'L';
+    }
+    else{
+        sizeName = 'XL';
     }
     return sizeName;
 }
@@ -17,6 +20,9 @@ void testTshirtSize() {
     std::cout << "\nTshirt size test\n";
     assert(size(37) == 'S');
     assert(size(40) == 'M');
+    assert(size(42) == 'M');
+    assert(size(38) == 'S');
     assert(size(43) == 'L');
+    aeert(size(48) == 'XL');
     std::cout << "All is well (maybe!)\n";
 }
